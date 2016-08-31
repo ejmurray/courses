0fa582113b1f71e3c45348423cf846d6e831b15d
Renato J. F <renatojen@gmail.com>
2016-08-31 12:46:01 -0300

    adding udemy course files

---
+++ udemy_python_mega_course/Script.py
@@ -0,0 +1,7 @@
+print('Hey there')
+replace='Ardit'.replace('A', 'O')
+print(replace)
+currency=145
+currency_today=100.5
+currency1='nothing'
+print(currency,currency_today,currency1)
---
+++ udemy_python_mega_course/ex01.py
@@ -0,0 +1,4 @@
+def cel2fahr(c):
+   return c*1.8+32
+
+print(cel2fahr(10))
